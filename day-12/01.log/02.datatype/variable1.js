a4cae7dd0975be1bb04c14af623a8550159973f8
Ankit <ankitsinghh04@gmail.com>
2024-06-08 13:29:00 +0530

    update

---
+++ day-12/01.log/02.datatype/variable1.js
@@ -0,0 +1,7 @@
+let a=5;
+var b=10;
+const c=15;
+console.log("a :",a);
+console.log("b :",b);
+console.log("c :",c);
+
---
+++ day-12/01.log/console.js
@@ -0,0 +1,6 @@
+
+let a=5,b=5,c;
+a=b+2+Math.random();
+b=a-1+Math.PI;
+c=a+b;
+console.log(c);
