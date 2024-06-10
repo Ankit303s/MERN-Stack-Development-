8894bfad99afe9ca89c46769ba974110f2231367
Ankit <ankitsinghh04@gmail.com>
2024-06-10 21:40:20 +0530

    gg

--- Day-14/assignment/asign.js
+++ Day-14/assignment/asign.js
@@ -81,3 +81,43 @@ console.log(string.concat(challenge, "."))
 
 //25
 console.log(challenge.repeat(2))
+
+//level - 2
+
+//1
+let statement1 = "The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' \
+ by John Holmes teaches us to help one another."
+ console.log(statement1)
+
+ //2
+ let statement2 = "Love is not patronizing and charity isn\'t about pity, it is about love. \
+Charity and love are the same -- with charity you give love, so don\'t just \
+give money but reach out your hand instead."
+console.log(statement2)
+
+//3
+let num1 = '10'
+let num2 = 10
+console.log(typeof(num1) == typeof(num2)) //false
+num1 = parseInt(num1)
+console.log(typeof(num1)) //Number
+
+//4
+let num3 = 9.8
+let num4 = 10
+console.log(num3 == num4) //false
+num3 = Math.round(num3)
+num3 = parseInt(num3)
+console.log(num3) //10
+
+//5
+let lan1 = 'python, jargon'
+console.log(lan1.match(/on/g))
+
+//6
+let sentence3 = "I hope this course is not full of jargon"
+console.log(sentence3.search('jargon'))
+
+
+//7
+console.log(Math.random()*100)
