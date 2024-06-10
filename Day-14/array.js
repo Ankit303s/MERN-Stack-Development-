e786f72f0daf0b609b664593244d59d7a9c75462
Ankit Singh <139601041+Ankit303s@users.noreply.github.com>
2024-06-10 13:52:35 +0530

    Add files via upload

---
+++ Day-14/array.js
@@ -0,0 +1,37 @@
+const arr1 = Array()
+console.log(arr1)
+
+const arr2 = new Array()
+console.log(arr2)
+
+const arr3 = []
+console.log(arr3)
+
+const numbers = [0, 3.14, 9.81, 37, 98.5, 100]
+const fruits = ['bananas', 'orange', 'mango', 'lomon']
+console.log('Numbers:', numbers)
+console.log('Numbers of numbers:', numbers.length)
+console.log('Fruits :',fruits)
+console.log('Numbers of fruits:',fruits.length)
+
+const arr4 = ['Ankit',250, true, 
+    {country: 'India', city: 'Kolkata'},
+    {skills: ['HTML', 'CSS', 'JS', 'React', 'Python']}
+]
+console.log(arr4)
+
+
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+console.log(charsInJavaScript)
+
+let companiesString = 'Facebook, Google Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+console.log(companies)
+
+
+let txt = 
+    'I love teaching and empowering people. I teach HTML, CSS ,JS, React, Python.'
+    const words = txt.split(' ')
+    console.log(words)
+
