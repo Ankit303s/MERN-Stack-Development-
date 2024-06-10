384d17542d060f98ce69fd3847a9b7e3401cee81
Ankit <ankitsinghh04@gmail.com>
2024-06-10 11:03:18 +0530

    update

---
+++ day-13/assignment/asign.js
@@ -0,0 +1,39 @@
+//1
+let challenge = 'World of JavaScript'
+
+//2
+console.log(challenge)
+
+//3
+console.log(challenge.length)
+
+//4
+console.log(challenge.toUpperCase())
+
+//5
+console.log(challenge.toLowerCase())
+
+//6
+console.log(challenge.substring(0,5))
+
+//7
+console.log(challenge.substring(7,19))
+
+//8
+console.log(challenge.includes("Script"))
+
+//9
+console.log(challenge.split(''))
+
+//10
+console.log(challenge.split(' '))
+
+//11
+let mnc =  'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(mnc.split(','))
+
+//12
+console.log(`Welcome to ${challenge.replace('JavaScript', 'Python')}`)
+
+//13
+console.log()
