58d56aacae3813dc0ffb84cffaff7b89f10a293f
Ankit <ankitsinghh04@gmail.com>
2024-06-11 11:25:33 +0530

    not completed

---
+++ Day-14/assignment/boolean op/boolean.js
@@ -0,0 +1,117 @@
+//level 1
+
+//1
+
+let firstName = 'Ankit'
+console.log(firstName)
+console.log(typeof firstName)
+
+let lastName = 'Singh'
+console.log(lastName)
+console.log(typeof lastName)
+
+let country = 'India'
+console.log(country)
+console.log(typeof country)
+
+let city = 'Kanpur'
+console.log(city)
+console.log(typeof city)
+
+let age = 20
+console.log(age)
+console.log(typeof age)
+
+let  marital_status= 'unmarried'
+console.log(marital_status)
+console.log(typeof marital_status)
+
+let year = 2024
+console.log(year)
+console.log(typeof year)
+
+//2
+let num1 = '10'
+let num2 = 10
+console.log(typeof num1 == typeof num2) //false
+
+//3
+let num3 = parseInt(9.8)
+let num4 = 10
+console.log(num3 == num4) //false
+
+//4 
+
+//true
+console.log(4 > 3)
+console.log(0 == '') 
+console.log(0 == false)
+//false
+console.log(5 < 3)
+console.log(0 === '')
+console.log(3 != 3) 
+
+//5
+
+let check = 4 > 3
+console.log(check)
+
+check = 4 >= 3
+console.log(check)
+
+check = 4 < 3
+console.log(check)
+
+check = 4 == 4
+console.log(check)
+
+check = 4 === 4
+console.log(check)
+
+check = (4 != 4)
+console.log(check)
+
+check = 4 !== 4
+console.log(check)
+
+check = 4 != '4'
+console.log(check)
+
+check = 4 == '4'
+console.log(check)
+
+check = 4 === '4'
+console.log(check)
+
+//6 
+
+check = 4 > 3 && 10 < 12
+console.log(check)
+
+check = 4 > 3 && 10 > 12
+console.log(check)
+
+check = 4 > 3 || 10 < 12
+console.log(check)
+
+check = 4 > 3 || 10 > 12
+console.log(check)
+
+check =  !(4 > 3)
+console.log(check)
+
+check = !(4 < 3)
+console.log(check)
+
+check = !(false)
+console.log(check)
+
+check =  !(4 > 3 && 10 < 12)
+console.log(check)
+
+check =  !(4 > 3 && 10 > 12)
+console.log(check)
+
+check =  !(4 === '4')
+console.log(check)
+
