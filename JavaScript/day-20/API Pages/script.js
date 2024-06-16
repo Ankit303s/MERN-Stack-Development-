8e3a40b7b78ec7c6cafdf7eb3c9b604dff61a9a8
Ankit Singh <139601041+Ankit303s@users.noreply.github.com>
2024-06-16 13:35:29 +0530

    Add files via upload

---
+++ JavaScript/day-20/API Pages/script.js
@@ -0,0 +1,27 @@
+//(function(){alert('Hello Everyone')})();
+const fetchData=()=>{
+    return fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((res)=>res.json())
+    .then((data)=>data.slice(0,10));
+};
+const getData=async()=>{
+    const data=await fetchData();
+    console.log('data:', data)
+    if(data !== undefined){
+        const mainbox=document.querySelector('.mainbox');
+        const timer=setInterval(()=>{
+            data.map((obj,index)=>{
+                const div=document.createElement('div');
+                div.className='box';
+                const h3=document.createElement('h3');
+                h3.innerHTML=`${obj.id}. ${obj.title}`;
+                div.appendChild(h3);
+                mainbox.appendChild(div)
+            });
+            setTimeout(()=>{
+                clearInterval(timer);
+            },1000)
+        },1000)
+    }
+}
+getData();
